975af589e27d66626bfa20f5550dff3a63ea3aeb
Perttu Moilanen <perttu.moilanen@alfame.com>
2023-04-10 21:38:22 +0300

    364 reactjs course first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://perttumoilanen:ynNIQjoEmDdVi2Bv@cluster0.dr4xrvm.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db("meetups");
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Head from "next/head";
+
+import MongoClient from "mongodb/lib/mongo_client";
+import MeetupList from "../components/meetups/MeetupList";
+import { Fragment } from "react";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta name='description'
+        content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://perttumoilanen:ynNIQjoEmDdVi2Bv@cluster0.dr4xrvm.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db("meetups");
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
